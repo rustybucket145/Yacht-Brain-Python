a98d39f1384f3086af2f8b276560821f466feeaf
Wade Powell <wade@wadet.com>
2019-01-01 19:49:15 -0800

    Changes to read sensors, update, delete, and CreateDatabase to only add lines if the tables are empty

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request
 
-
+print('top of script')
 
 from sqlalchemy import event, DDL
 
@@ -21,6 +21,7 @@ from models import Sensors, SensorLocations, SensorTypes
 
 @app.route('/')
 def index():
+    print('starting index')
     return render_template("index.html")
 
 @app.route('/create_database')
@@ -41,12 +42,26 @@ def create_database():
 
 @app.route('/sensors', methods=["GET", "POST"])
 def sensors():
-
+    print('starting sensors')
+   
+    print('starting SensorsQuery')
+    #get all sensors stored in the database
+    sensors = Sensors.query.order_by(Sensors.title).all()
+    
+    print('starting loop through sensors in DB to build array')
+    #loop through all sensors saved in db and create an array of the sensor id's
+    sensors_in_database = []
+    for sensor in sensors:
+        sensors_in_database.append(sensor.sensor_id)
+    
     #if a form is posted we add the data
     if request.form:
+        print('form was posted')
         sensor_db_id = request.form.get("sensor_db_id")
 
+        # its adding a new one on update... change to do an update        
         sensor = Sensors(title = request.form.get("title")
+        , sensor_db_id = request.form.get("sensor_db_id")
         , sensor_id = request.form.get("sensor_id")
         , sensor_location = request.form.get("sensor_location")
         , sensor_type = request.form.get("sensor_type")
@@ -62,57 +77,61 @@ def sensors():
         , is_analog_sensor = request.form.get("is_analog_sensor"))
         
         # if this sensor already exists in the db we update
-        if sensor_db_id:
+        if sensor_db_id is not '':
             #update record
-            
-            sensor.sensor_db_id = sensor_db_id
-            db.session.merge(sensor)
-            db.session.commit()
-            
-        else:
-            #insert record   
+            print('form is being updated')
+            #sensor.sensor_db_id = sensor_db_id
+        
+            if request.form.get("delete"):
+                #delete record
+                Sensors.query.filter_by(sensor_db_id=request.form.get("sensor_db_id")).delete()
+                db.session.commit()
+            else:
+                #update the changes
+                db.session.merge(sensor)
+                db.session.commit()
+        
+        elif request.form.get("sensor_id") not in sensors_in_database:
+            #insert record
+            print('Sensor Count in merge:' + str(Sensors))    
+            print('sensor is being inserted')
             db.session.add(sensor)
             db.session.commit()
+            sensors_in_database.append(sensor.sensor_id)
+               
         
-
     #here we have the normal behavior of the page
     #get all the sensors in database
     #get all sensors on the bus
     #compare the list of each against each other, place the non-assigned sensors at the top of the page maybe?
      
+    print('starting sensor locations')
     #get all the possible sensor locations
     sensor_locations = SensorLocations.query.order_by(SensorLocations.sensor_location_value).all()
     #get all the possible sensor types
     sensor_types = SensorTypes.query.order_by(SensorTypes.sensor_type_value).all()
     
     from lib import OneWireSensors
-
+    print('starting import from OneWireSensors')
     #get all sensors off the onewire bus
     sensor_ids_onewire = OneWireSensors.get_all_onewire_ids()
-
-    #get all sensors stored in the database
-    sensors = Sensors.query.order_by(Sensors.title).all()
-
-    #loop through all sensors saved in db and create an array of the sensor id's
-    sensors_in_database = []
-    for sensor in sensors:
-        sensors_in_database.append(sensor.sensor_id)
-
     
-
+   
+    print('starting loop through onewireids')
     for sensor_id in sensor_ids_onewire:
         if sensor_id not in sensors_in_database:
-            print('not in array')
-            #sensors.update({'sensor_id':sensor_id})
-            #sensors[sensor_id] = []
-            #sensors[sensor_id]['sensor_id'] = sensor_id
+            sensor = Sensors(sensor_id = sensor_id)
+            #insert record   
+            db.session.add(sensor)
+            db.session.commit()
             
-    print(sensors)
 
+    #pick up the changes before returning to page
+    sensors = Sensors.query.order_by(Sensors.title).all()
+    
+    print('Finished loops')
     
     return render_template("sensors.html", sensors = sensors, sensor_locations=sensor_locations,sensor_types=sensor_types)
-
-
 
       
 
--- database_create.py
+++ database_create.py
@@ -8,45 +8,51 @@ from shared import db
 from models import Sensors, SensorLocations, SensorTypes
 
 
-
+          
 
 def insert_initial_values_sensor_locations():
     print('firing initial values for sensor locations')
-    db.session.add(SensorLocations(sensor_location_value='', sensor_location_title='--select--'))
-    db.session.add(SensorLocations(sensor_location_value='engine_single', sensor_location_title='Single Engine'))
-    db.session.add(SensorLocations(sensor_location_value='engine_port', sensor_location_title='Port Engine'))
-    db.session.add(SensorLocations(sensor_location_value='engine_stbd', sensor_location_title='Starboard Engine'))
-    db.session.add(SensorLocations(sensor_location_value='generator1', sensor_location_title='Generator 1'))
-    db.session.add(SensorLocations(sensor_location_value='generator2', sensor_location_title='Generator 2'))
-    db.session.add(SensorLocations(sensor_location_value='salon', sensor_location_title='Salon'))
-    db.session.add(SensorLocations(sensor_location_value='galley', sensor_location_title='Galley'))
-    db.session.add(SensorLocations(sensor_location_value='bridge', sensor_location_title='Bridge'))
-    db.session.add(SensorLocations(sensor_location_value='cockpit', sensor_location_title='Cockpit'))
-    db.session.add(SensorLocations(sensor_location_value='engine_room', sensor_location_title='Engine Room'))
-    db.session.add(SensorLocations(sensor_location_value='refrigeration', sensor_location_title='Referigeration'))
-    db.session.add(SensorLocations(sensor_location_value='livewell', sensor_location_title='Livewell'))
-    db.session.commit()
+    
+    #check to see if these have been populated already
+    if db.session.query(SensorLocations.sensor_location_title).all() == []:
+        db.session.add(SensorLocations(sensor_location_value='', sensor_location_title='--select--'))
+        db.session.add(SensorLocations(sensor_location_value='engine_single', sensor_location_title='Single Engine'))
+        db.session.add(SensorLocations(sensor_location_value='engine_port', sensor_location_title='Port Engine'))
+        db.session.add(SensorLocations(sensor_location_value='engine_stbd', sensor_location_title='Starboard Engine'))
+        db.session.add(SensorLocations(sensor_location_value='generator1', sensor_location_title='Generator 1'))
+        db.session.add(SensorLocations(sensor_location_value='generator2', sensor_location_title='Generator 2'))
+        db.session.add(SensorLocations(sensor_location_value='salon', sensor_location_title='Salon'))
+        db.session.add(SensorLocations(sensor_location_value='galley', sensor_location_title='Galley'))
+        db.session.add(SensorLocations(sensor_location_value='bridge', sensor_location_title='Bridge'))
+        db.session.add(SensorLocations(sensor_location_value='cockpit', sensor_location_title='Cockpit'))
+        db.session.add(SensorLocations(sensor_location_value='engine_room', sensor_location_title='Engine Room'))
+        db.session.add(SensorLocations(sensor_location_value='refrigeration', sensor_location_title='Referigeration'))
+        db.session.add(SensorLocations(sensor_location_value='livewell', sensor_location_title='Livewell'))
+        db.session.commit()
     
 def insert_initial_values_sensor_types():
     print('firing initial values for sensor types')
-    db.session.add(SensorTypes(sensor_type_value='', sensor_type_title='--select--'))
-    db.session.add(SensorTypes(sensor_type_value='rpm', sensor_type_title='Livewell'))
-    db.session.add(SensorTypes(sensor_type_value='coolantTemperature', sensor_type_title='Coolant Temp'))
-    db.session.add(SensorTypes(sensor_type_value='oilTemperature', sensor_type_title='Oil Temp'))
-    db.session.add(SensorTypes(sensor_type_value='transmissionOilTemperature', sensor_type_title='Transmission Oil Temp'))
-    db.session.add(SensorTypes(sensor_type_value='exhaustTemperature', sensor_type_title='EGT'))
-    db.session.add(SensorTypes(sensor_type_value='exhaustTemperatureInboard', sensor_type_title='EGT Inboard'))
-    db.session.add(SensorTypes(sensor_type_value='exhaustTemperatureOutboard', sensor_type_title='EGT Outboard'))
-    db.session.add(SensorTypes(sensor_type_value='alternatorVoltage', sensor_type_title='Alternator Voltage'))
-    db.session.add(SensorTypes(sensor_type_value='oilPressure', sensor_type_title='Oil Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='coolantPressure', sensor_type_title='Coolant Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='boostPressure', sensor_type_title='Boost Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='transmissionOilPressure', sensor_type_title='Transmission Oil Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='fuelPressure', sensor_type_title='Fuel Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='crankcasePressure', sensor_type_title='Crankcase Pressure'))
-    db.session.add(SensorTypes(sensor_type_value='runTime', sensor_type_title='Run Time')) 
-    db.session.commit()
     
+    #check to see if these have been populated already
+    if db.session.query(SensorTypes.sensor_type_title).all() == []:
+        db.session.add(SensorTypes(sensor_type_value='', sensor_type_title='--select--'))
+        db.session.add(SensorTypes(sensor_type_value='rpm', sensor_type_title='Livewell'))
+        db.session.add(SensorTypes(sensor_type_value='coolantTemperature', sensor_type_title='Coolant Temp'))
+        db.session.add(SensorTypes(sensor_type_value='oilTemperature', sensor_type_title='Oil Temp'))
+        db.session.add(SensorTypes(sensor_type_value='transmissionOilTemperature', sensor_type_title='Transmission Oil Temp'))
+        db.session.add(SensorTypes(sensor_type_value='exhaustTemperature', sensor_type_title='EGT'))
+        db.session.add(SensorTypes(sensor_type_value='exhaustTemperatureInboard', sensor_type_title='EGT Inboard'))
+        db.session.add(SensorTypes(sensor_type_value='exhaustTemperatureOutboard', sensor_type_title='EGT Outboard'))
+        db.session.add(SensorTypes(sensor_type_value='alternatorVoltage', sensor_type_title='Alternator Voltage'))
+        db.session.add(SensorTypes(sensor_type_value='oilPressure', sensor_type_title='Oil Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='coolantPressure', sensor_type_title='Coolant Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='boostPressure', sensor_type_title='Boost Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='transmissionOilPressure', sensor_type_title='Transmission Oil Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='fuelPressure', sensor_type_title='Fuel Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='crankcasePressure', sensor_type_title='Crankcase Pressure'))
+        db.session.add(SensorTypes(sensor_type_value='runTime', sensor_type_title='Run Time')) 
+        db.session.commit()
+        
 #event.listen(SensorLocations.__table__, 'after_create', insert_initial_values_sensor_locations)
 
 
