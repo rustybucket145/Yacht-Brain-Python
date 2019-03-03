6441978d265910170e3979de9b1de9bade76cfb8
Wade Powell <wade@wadet.com>
2019-03-03 12:00:19 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+#sys.path.append('/Adafruit_Python_GPIO')
 import os
 import sys
-#sys.path.append('/Adafruit_Python_GPIO')
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import lib.Adafruit_GPIO.SPI as SPI
@@ -9,19 +9,12 @@ import lib.Adafruit_MAX31855.MAX31855 as MAX31855
 import time
 import glob
 
-base_dir = '/sys/bus/w1/devices/'
-
 from flask import Flask, render_template, request
-
-print('top of script')
-
 from sqlalchemy import event, DDL
 
-
+base_dir = '/sys/bus/w1/devices/'
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir,"yachtbraindatabase.db"))
-
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
@@ -32,22 +25,18 @@ db.init_app(app)
 from models import RPIPins, Sensors, SensorLocations, SensorTypes
 
 
-@app.route('/')
-def index():
-    print('starting index')
-    return render_template("index.html")
-
 # Define a function to convert celsius to fahrenheit.
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
-
+#used for 1Wire 
 def read_temp_raw(device_file):
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
+#used for 1Wire
 def read_temp(sensor_id):
     device_folder = glob.glob(base_dir + sensor_id)[0]
     device_file = device_folder + '/w1_slave'
@@ -63,21 +52,30 @@ def read_temp(sensor_id):
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return int(round(temp_f))
 
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+# -  PAGE Functions
+
+@app.route('/')
+def index():
+    print('starting index')
+    return render_template("index.html")
+
+
 @app.route('/create_database')
 def create_database():
     from database_create import insert_initial_values_sensor_locations, insert_initial_values_sensor_types, insert_initial_RPIValues
-    print('pre listen')
-    #event.listen(SensorLocations.__table__, 'after_create', insert_initial_values_sensor_locations)
-    #event.listen(SensorTypes.__table__, 'after_create', insert_initial_values_sensor_types)
- 
-    print('pre create')
+
     db.create_all()
-    print('post create')
 
     insert_initial_values_sensor_locations()
     insert_initial_values_sensor_types()
     insert_initial_RPIValues()
-    print('post values')
+    
     return render_template("index.html")
 
 
@@ -99,8 +97,8 @@ def sensors():
         print('form was posted')
         sensor_db_id = request.form.get("sensor_db_id")
 
-        #        , sensor_id = request.form.get("sensor_id")  - removed this so they cant overwrite it
-        # its adding a new one on update... change to do an update        
+        # , sensor_id = request.form.get("sensor_id")  - removed this so they cant overwrite it - used for sensor reads
+        # read values from form
         sensor = Sensors(title = request.form.get("title")
         , sensor_db_id = request.form.get("sensor_db_id")
         , sensor_location = request.form.get("sensor_location")
@@ -119,9 +117,6 @@ def sensors():
         # if this sensor already exists in the db we update
         if sensor_db_id is not '':
             #update record
-            print('form is being updated')
-            #sensor.sensor_db_id = sensor_db_id
-        
             if request.form.get("delete"):
                 #delete record
                 Sensors.query.filter_by(sensor_db_id=request.form.get("sensor_db_id")).delete()
@@ -132,9 +127,7 @@ def sensors():
                 db.session.commit()
         
         elif request.form.get("sensor_id") not in sensors_in_database:
-            #insert record
-            print('Sensor Count in merge:' + str(Sensors))    
-            print('sensor is being inserted')
+            #its a new one, insert record
             db.session.add(sensor)
             db.session.commit()
             sensors_in_database.append(sensor.sensor_id)
@@ -154,8 +147,6 @@ def sensors():
     #Now, sense all 1 Wires and Add them to DB
     from lib import OneWireSensors
     
-    print('starting import from OneWireSensors')
-    
     #get all sensors off the onewire bus
     sensor_ids_onewire = OneWireSensors.get_all_onewire_ids()
        
@@ -166,11 +157,7 @@ def sensors():
             db.session.add(sensor)
             db.session.commit()
     
-    #from lib import Adafruit_MCP3008
-    
     #Now, sense any data on the Analog Sensors and Add them to DB
-    # Software SPI configuration:
-    
     for  rpipin in rpipins:
         if rpipin.rpipins_type == "Analog" and rpipin.rpipins_clk is not None and rpipin.rpipins_clk != 'None':
             
@@ -183,7 +170,6 @@ def sensors():
             mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
             print('Inspecting Analog for data')
-        
             # Read all the ADC channel values in a list.
             values = [0]*8
             for i in range(8):
@@ -212,19 +198,18 @@ def sensors():
             CS   = int(rpipin.rpipins_cs)
             maxsensor = MAX31855.MAX31855(CLK, CS, DO)
 
-            
             sensor = Sensors(sensor_id = 'MAX31855-' + str(x))
-            #print('SensorID: ' + sensor)
+
             if sensor.sensor_id not in sensors_in_database:
                 #insert record   
                 db.session.add(sensor)
                 db.session.commit()
             
             #example code on reading from sensor
-            temp = maxsensor.readTempC()
-            internal = maxsensor.readInternalC()
-            print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
-            print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
+##            temp = maxsensor.readTempC()
+##            internal = maxsensor.readInternalC()
+##            print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
+##            print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
                
                     
     
@@ -239,23 +224,16 @@ def sensors():
 
 @app.route('/tables', methods=["GET", "POST"])
 def rpipins():
-    print('start table function')
-    
+
     rpipins = RPIPins.query.order_by(RPIPins.rpipins_title).all()
-    
-    print('Load rpipins into Array')
+
     #loop through all sensors saved in db and create an array of the sensor id's
-    
     for rpipin in rpipins:
         #sensors_in_database.append(sensor.sensor_id)
         print('instantiated rpipin')
     
-    print('madeit')
     if request.form:
-        print('request.form')
-        #not triggering.. maybe put empty one there???
         rpipins_title = str(request.form.get("rpipins_title"))
-        print(rpipins_title)
         if rpipins_title != '':
             #do the update. 
             print('updating values')
@@ -271,32 +249,20 @@ def rpipins():
             print('rpi pin values set')
             db.session.merge(rpipin)
             db.session.commit()
-        
-        
             
-            
-    print('post values')
     return render_template("tables.html", rpipins = rpipins)
 
-def left(s, amount):
-    return s[:amount]
-
-def right(s, amount):
-    return s[-amount:]
 
 @app.route('/display')
 def display():
 
-    print('Reading Sensors from DB')
     #get all sensors stored in the database
     sensors = Sensors.query.order_by(Sensors.title).all()
     
     ScreenText = []
 
-    print('Load DB Sensors into Array')
     #loop through all sensors saved in db and create an array of the sensor id's
     for sensor in sensors:
-
                
         #Display them all now to screen
         if sensor.is_one_wire == 1:
